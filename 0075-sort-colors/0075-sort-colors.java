7679fc8c75a0c4edbb5fd4be6f67842bcf1edae9
Pratik Raj <145989611+pratikraj143@users.noreply.github.com>
2025-07-06 15:43:02 +0530

    Time: 0 ms (100%), Space: 41.6 MB (96.99%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.java
@@ -0,0 +1,29 @@
+class Solution {
+    public void sortColors(int[] nums) {
+       int zero=0,one=0,two=0;
+       //count each Occurence
+       for(int i=0;i<nums.length;i++){
+        if(nums[i]==0){
+            zero++;
+        }else if(nums[i]==1){
+            one++;
+        }else{
+            two++;
+        }
+       }
+        //overwrite
+        for(int i=0;i<nums.length;i++){
+            if(zero !=0){
+                nums[i] =0;
+                zero--;
+            }else if(one !=0){
+                nums[i] =1;
+                one--;
+            }else{
+                nums[i]=2;
+                two--;
+            }
+            
+        }
+    }
+}
