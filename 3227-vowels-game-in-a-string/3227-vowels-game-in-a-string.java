5b877199d9f11f26c51fc4a86e07490305fd5283
Pratik Raj <145989611+pratikraj143@users.noreply.github.com>
2025-09-12 21:32:27 +0530

    Time: 8 ms (45.41%), Space: 46.1 MB (6.04%) - LeetHub

--- 3227-vowels-game-in-a-string/3227-vowels-game-in-a-string.java
+++ 3227-vowels-game-in-a-string/3227-vowels-game-in-a-string.java
@@ -4,12 +4,9 @@ class Solution {
         for(int i=0;i<s.length();i++){
             if(s.charAt(i)=='a'|| s.charAt(i)=='e'|| s.charAt(i)=='i'||
                 s.charAt(i)=='o'||s.charAt(i)=='u'){
-                    count++;
+                   return true;
             }
         }
-        if(count==0){
-            return false;
-        }
-        return true;
+        return false;        
     }
 }
