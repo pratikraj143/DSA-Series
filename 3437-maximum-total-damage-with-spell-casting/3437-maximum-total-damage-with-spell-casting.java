7b9789decc31df9c9284a53c6b380165d13a37e5
Pratik Raj <145989611+pratikraj143@users.noreply.github.com>
2025-10-11 16:45:39 +0530

    Time: 52 ms (100%), Space: 61.2 MB (80.18%) - LeetHub

--- 3437-maximum-total-damage-with-spell-casting/3437-maximum-total-damage-with-spell-casting.java
+++ 3437-maximum-total-damage-with-spell-casting/3437-maximum-total-damage-with-spell-casting.java
@@ -1,28 +1,26 @@
 class Solution {
-
     public long maximumTotalDamage(int[] power) {
-        TreeMap<Integer, Integer> count = new TreeMap<>();
-        for (int p : power) {
-            count.put(p, count.getOrDefault(p, 0) + 1);
-        }
-        List<int[]> vec = new ArrayList<>();
-        vec.add(new int[] { -1000000000, 0 });
-        for (Map.Entry<Integer, Integer> e : count.entrySet()) {
-            vec.add(new int[] { e.getKey(), e.getValue() });
-        }
-        int n = vec.size();
-        long[] f = new long[n];
-        long mx = 0;
-        long ans = 0;
-        int j = 1;
-        for (int i = 1; i < n; i++) {
-            while (j < i && vec.get(j)[0] < vec.get(i)[0] - 2) {
-                mx = Math.max(mx, f[j]);
-                j++;
+        Arrays.sort(power);
+
+        long[] dp = new long[power.length];
+        long md = 0;
+        dp[0] = power[0];
+        for (int i = 1, j = 0; i < power.length; i++) {
+            if (power[i] == power[i-1]) {
+                dp[i] = dp[i-1]+power[i];
+            } else {
+                while (power[j]+2 < power[i]) {
+                    md = Math.max(md, dp[j]);
+                    j++;
+                }
+                dp[i] = md + power[i];
             }
-            f[i] = mx + 1L * vec.get(i)[0] * vec.get(i)[1];
-            ans = Math.max(ans, f[i]);
         }
-        return ans;
+        
+        long max = 0;
+        for (long n : dp) {
+            max = Math.max(max, n);
+        }
+        return max;
     }
 }
